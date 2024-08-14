a81c82dbcebc4d1224e0a563ca1669cc810dfd01
mickay <michealaderibigbe@gmail.com>
2024-08-14 22:31:57 +0100

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,13 +5,26 @@ Contains the number_of_subscribers function
 
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'myRedditApp/0.1'}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        
+        # Check for redirect status
+        if response.status_code == 302:
+            return 0
+        
+        # Check for successful response
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        
+        # If the response code is not 200 or 302, return 0
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/efaeleng)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    
+    except requests.RequestException:
+        # Catch any exceptions related to the request
+        return 0
+
