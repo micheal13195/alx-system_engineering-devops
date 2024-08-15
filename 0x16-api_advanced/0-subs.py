018827827ca874188209614a45af64e1ae99e3df
mickay <michealaderibigbe@gmail.com>
2024-08-15 09:22:47 +0100

    API

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-	"""returns the number of subscribers for a given subreddit"""
-	if subreddit is None or type(subreddit) is not str:
-		return 0
-	r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-			headers={'User-Agent': '0x16-api_advanced:project:\
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
 v1.0.0 (by /u/efaeleng)'}).json()
-	subs = r.get("data", {}).get("subscribers", 0)
-	
+    subs = r.get("data", {}).get("subscribers", 0)
+
